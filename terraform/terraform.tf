555726fd53a1ed89196bea6a04269e1399f5add3
Yash Khemani <yash@khemani.com>
2024-01-04 22:10:03 +0000

    refine versions

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,18 +1,19 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
 
     hcp = {
       source  = "hashicorp/hcp"
-      version = ">= 0.71.0"
+      version = "~> 0.79"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = ">= 4.0.5"
+      version = "~> 4.0"
     }
   }
-  required_version = ">= 1.6"
+  required_version = "~> 1.6"
 }
