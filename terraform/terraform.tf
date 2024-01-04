dc3288654d994dc37dc48384fe489629908b475b
Yash Khemani <yash@khemani.com>
2024-01-04 14:11:40 +0000

    remove doormat provider

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -13,11 +13,6 @@ terraform {
       source  = "hashicorp/tls"
       version = ">= 4.0.5"
     }
-
-    doormat = {
-      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
-      version = "~> 0.0.2"
-    }
   }
-  required_version = ">= 1.5"
+  required_version = ">= 1.6"
 }
