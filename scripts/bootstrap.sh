8fed96305de48a7d6460c6bf876134531c323756
Yash Khemani <yash@khemani.com>
2024-01-30 14:04:12 +0000

    bootstrap++

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,7 +6,7 @@ _log() {
   # accept argument string or stdin
   local text="$*"; if [ "$#" -eq 0 ]; then text="$(cat)"; fi
   local dt; dt="$(date --rfc-3339=seconds)"
-  printf '%s [%s] [Entrypoint]: %s\n' "$dt" "$type" "$text"
+  printf '%s [%s] [bootstrap]: %s\n' "$dt" "$type" "$text"
 }
 
 _info() {
@@ -20,9 +20,187 @@ _error() {
   exit 1
 }
 
-if [ "$SSH_IMPORT_ID" == "" ]
+_info "Starting bootstrap.sh"
+
+########################################################################
+#
+# ssh keys
+#
+
+# RSA private key
+if [ "$SSH_HOST_RSA_KEY" == "" ]
 then
-  _error "SSH_IMPORT_ID is not set"
+  _error "SSH_HOST_RSA_KEY is not set"
 else
-  _info "SSH_IMPORT_ID is $SSH_IMPORT_ID"
+  _info "Setting host RSA private key"
+  echo $SSH_HOST_RSA_KEY > /etc/ssh/ssh_host_rsa_key
 fi
+
+# RSA public key
+if [ "$SSH_HOST_RSA_KEY_PUB" == "" ]
+then
+  _error "SSH_HOST_RSA_KEY_PUB is not set"
+else
+  _info "Setting host RSA public key"
+  echo $SSH_HOST_RSA_KEY_PUB > /etc/ssh/ssh_host_rsa_key.pub
+fi
+
+# ECDSA private key
+if [ "$SSH_HOST_ECDSA_KEY" == "" ]
+then
+  _error "SSH_HOST_ECDSA_KEY is not set"
+else
+  _info "Setting host ECDSA private key"
+  echo $SSH_HOST_ECDSA_KEY > /etc/ssh/ssh_host_ecdsa_key
+fi
+
+# ECDSA public key
+if [ "$SSH_HOST_ECDSA_KEY_PUB" == "" ]
+then
+  _error "SSH_HOST_ECDSA_KEY_PUB is not set"
+else
+  _info "Setting host ECDSA public key"
+  echo $SSH_HOST_ECDSA_KEY_PUB > /etc/ssh/ssh_host_ecdsa_key.pub
+fi
+
+# ED25519 private key
+if [ "$SSH_HOST_ED25519_KEY" == "" ]
+then
+  _error "SSH_HOST_ED25519_KEY is not set"
+else
+  _info "Setting host ED25519 private key"
+  echo $SSH_HOST_ED25519_KEY > /etc/ssh/ssh_host_ed25519_key
+fi
+
+# ED25519 public key
+if [ "$SSH_HOST_ED25519_KEY_PUB" == "" ]
+then
+  _error "SSH_HOST_ED25519_KEY_PUB is not set"
+else
+  _info "Setting host ED25519 public key"
+  echo $SSH_HOST_ED25519_KEY_PUB > /etc/ssh/ssh_host_ed25519_key.pub
+fi
+
+_info "Restarting ssh daemon"
+systemctl restart ssh
+
+if [ "$SSH_IMPORT_ID" != "" ]
+then
+  _info "Importing ssh keys for $SSH_IMPORT_ID"
+  ssh-import-id $SSH_IMPORT_ID
+fi
+
+# Ubuntu docker privs
+_info "Adding ubuntu user to docker group"
+usermod -aG docker ubuntu
+
+# auto-completions
+_info "Configuring auto-completion for command commands"
+cat <<EOF >> /root/.bashrc
+complete -C /usr/bin/vault vault
+complete -C /usr/bin/consul consul
+complete -C /usr/bin/terraform terraform
+complete -C /usr/bin/nomad nomad
+complete -C /usr/bin/packer packer
+EOF
+
+# PKI
+_info "Creating TLS certificate files"
+mkdir -p $CERT_DIR/wildcard
+
+if [ "$WILDCARD_PRIVATE_KEY" == "" ]
+then
+  _error "WILDCARD_PRIVATE_KEY is not set"
+else
+  _info "Writing WILDCARD_PRIVATE_KEY to $CERT_DIR/wildcard/privkey.pem"
+  echo $WILDCARD_PRIVATE_KEY > $CERT_DIR/wildcard/privkey.pem
+fi
+
+if [ "$WILDCARD_CERT" == "" ]
+then
+  _error "WILDCARD_CERT is not set"
+else
+  _info "Writing WILDCARD_CERT to $CERT_DIR/wildcard/cert.pem"
+  echo $WILDCARD_CERT > $CERT_DIR/wildcard/cert.pem
+fi
+
+if [ "$CA_CERT" == "" ]
+then
+  _error "CA_CERT is not set"
+else
+  _info "Writing CA_CERT to $CERT_DIR/wildcard/ca.pem"
+  echo $CA_CERT > $CERT_DIR/wildcard/ca.pem
+  _info "Writing CA_CERT to /usr/local/share/ca-certificates/demo-ca.pem"
+  echo $CA_CERT > /usr/local/share/ca-certificates/demo-ca.pem
+fi
+
+_info "Writing wildcard private key and cert to $CERT_DIR/wildcard/privkey_cert.pem"
+cat $CERT_DIR/wildcard/privkey.pem > $CERT_DIR/wildcard/privkey_cert.pem
+cat $CERT_DIR/wildcard/cert.pem >> $CERT_DIR/wildcard/privkey_cert.pem
+
+_info "Writing private key, cert and ca cert (fullchain) to $CERT_DIR/wildcard/bundle.pem"
+cat $CERT_DIR/wildcard/privkey.pem > $CERT_DIR/wildcard/bundle.pem
+cat $CERT_DIR/wildcard/cert.pem >> $CERT_DIR/wildcard/bundle.pem
+cat $CERT_DIR/wildcard/ca.pem >> $CERT_DIR/wildcard/bundle.pem
+
+_info "Running update-ca-certificates"
+update-ca-certificates
+
+# Vault
+_info "Creating directories for Vault to run in docker"
+mkdir -p \
+  /data/vault/data \
+  /data/vault/conf \
+  /data/vault/audit \
+  /data/vault/plugins \
+  /data/vault/license
+
+if [ "$VAULT_LICENSE" == "" ]
+then
+  _error "VAULT_LICENSE is not set"
+else
+  _info "Writing VAULT_LICENSE to /data/vault/license/vault.hclic"
+  echo $VAULT_LICENSE > /data/vault/license/vault.hclic
+fi
+
+if [ "$DOMAIN" == "" ]
+then
+  _error "DOMAIN is not set"
+fi
+
+_info "Writing Vault config to /data/vault/conf/vault.hcl"
+cat <<EOF > /data/vault/conf/vault.hcl
+# raft storage
+storage "raft" {
+  path    = "/vault/data"
+  node_id = "node_1"
+
+ retry_join {
+    leader_api_addr = "https://vault.$DOMAIN:8200"
+    leader_ca_cert_file = "/run/secrets/wildcard_ca_cert"
+    tls_cert_file = "/run/secrets/wildcard_cert"
+    tls_key_file = "/run/secrets/wildcard_privkey"
+  }
+}
+
+listener "tcp" {
+  address       = "0.0.0.0:8200"
+  tls_cert_file = "/run/secrets/wildcard_fullchain"
+  tls_key_file  = "/run/secrets/wildcard_privkey"
+}
+
+api_addr     = "https://vault.$DOMAIN:8200"
+cluster_addr = "https://vault.$DOMAIN:8201"
+
+cluster_name = "vault"
+EOF
+
+_info "Setting owner/group for /data/vault"
+chown -R 100:1000 /data/vault
+
+# Docker
+_info "Enable and start docker"
+sudo systemctl enable docker && \
+sudo systemctl start docker
+
+_info "Finished bootstrap.sh"
